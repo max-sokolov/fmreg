17f60954d2965ebc3aa621880f58a992766b93f2
Maxim Sokolov <sokolov.maximus@gmail.com>
2016-08-22 12:06:47 -0400

    Add util function mutate_cs() that mutates each cross-section of the data

---
+++ R/f_mutate_cs.R
@@ -0,0 +1,41 @@
+###############################################################################
+# (c) Maxim Sokolov
+###############################################################################
+
+mutate_cs <- function(.data, vars, date_var, method, cutoffs,
+                      out_vars = vars){
+  # _________________ check arguments ______________________
+  stopifnot(are_characters(vars, date_var, method))
+
+  stopifnot(all(c(vars, date_var) %in% colnames(.data)))
+
+  stopifnot(length(date_var) == 1)
+
+  stopifnot(length(vars) == length(out_vars))
+
+  # ________________ choose the method _____________________
+  if (method == "winsorize"){
+    f_mutate <- quote(portfs::winsorize)
+  } else if (method == "trim"){
+    f_mutate <- quote(portfs::trim)
+  } else {
+    stop("The `method` argument is illegal")
+  }
+
+  # standard evaluation machinery for mutate_
+  dots <- list()
+  for (i in seq_along(vars)){
+    dots[[ out_vars[i] ]] <- substitute(f_mutate(x, cutoffs = cutoffs),
+                                        env = list(x = as.name(vars[i]),
+                                                   cutoffs = cutoffs,
+                                                   f_mutate = f_mutate))
+  }
+
+  df_data_out <- dplyr::group_by_(.data, .dots = list(as.name(date_var)))
+
+  df_data_out <- dplyr::mutate_(df_data_out, .dots = dots)
+
+  df_data_out <- dplyr::ungroup(df_data_out)
+
+  df_data_out
+}
---
+++ tests/testthat/test_f_mutate_cs.R
@@ -0,0 +1,74 @@
+###############################################################################
+# (c) Maxim Sokolov
+###############################################################################
+
+# _____________________________________________________________________________
+context("Main check for mutate_cs()")
+
+f <- mutate_cs
+
+winsorize <- portfs::winsorize
+trim      <- portfs::trim
+
+data_frame <- dplyr::data_frame
+
+test_that("f gives right answers in the simplest case", {
+  # 1000 firms, 1 period
+  n    <- 1000
+
+  df_data <- data_frame(date = rep(1L, times = n),
+                        a    = seq(1, n))
+
+  cutoffs = c(0.01, 0.99)
+
+  # winsorization
+  df_res <- dplyr::mutate(df_data, a = winsorize(a, cutoffs))
+
+  expect_identical(f(df_data, vars = "a", date_var = "date",
+                     method = "winsorize", cutoffs = cutoffs),
+                   df_res)
+
+  # trimming
+  df_res <- dplyr::mutate(df_data, a = trim(a, cutoffs))
+
+  expect_identical(f(df_data, vars = "a", date_var = "date",
+                     method = "trim", cutoffs = cutoffs),
+                   df_res)
+})
+
+test_that("f gives right answers in a bit complicated case", {
+  # 1000 firms, 2 period
+  n    <- 1000
+
+  v_rand <- rnorm(n)
+
+  df_data <- data_frame(date = c(rep(1L, times = n), rep(2L, times = n)),
+                        a    = c(seq(1, n),          2*seq(1, n)),
+                        b    = c(v_rand,             v_rand))
+
+  cutoffs = c(0.01, 0.99)
+
+  # winsorization
+  df_res <- dplyr::mutate(df_data,
+                          a = c(winsorize(seq(1, n),   cutoffs),
+                                winsorize(2*seq(1, n), cutoffs)),
+                          b = c(winsorize(v_rand, cutoffs),
+                                winsorize(v_rand, cutoffs))
+                         )
+
+  expect_identical(f(df_data, vars = c("a", "b"), date_var = "date",
+                     method = "winsorize", cutoffs = cutoffs),
+                   df_res)
+
+  # trimming
+  df_res <- dplyr::mutate(df_data,
+                          a = c(trim(seq(1, n),   cutoffs),
+                                trim(2*seq(1, n), cutoffs)),
+                          b = c(trim(v_rand, cutoffs),
+                                trim(v_rand, cutoffs))
+                         )
+
+  expect_identical(f(df_data, vars = c("a", "b"), date_var = "date",
+                     method = "trim", cutoffs = cutoffs),
+                   df_res)
+})
