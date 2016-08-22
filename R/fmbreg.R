0d1f28c35e74acb16b925f43fb181ace74bfc089
Maxim Sokolov <sokolov.maximus@gmail.com>
2016-08-22 11:22:20 -0400

    Keep refactoring fmbreg: rewrite construction of the cross-sectional data frame and the Fama-MacBeth data frame

--- R/fmbreg.R
+++ R/fmbreg.R
@@ -25,6 +25,7 @@
 
 #' @export
 fmbreg <- function(.data, y, X, date_var, intercept = TRUE){
+
   # ___________________ check arguments ____________________
   if (are_characters(y, X, date_var) == FALSE){
     stop("Arguments y, X, and date_var need to be character vectors.")
@@ -60,7 +61,7 @@ fmbreg <- function(.data, y, X, date_var, intercept = TRUE){
     X_aug <- c("(Intercept)", X)
   }
 
-  k <- length(X_aug)
+  n_regressors <- length(X_aug)
 
   # __________________ make unique dates ___________________
   v_dates <- sort(unique(.data[[date_var]]))
@@ -78,59 +79,71 @@ fmbreg <- function(.data, y, X, date_var, intercept = TRUE){
       warning("Date ", tmp_date, " contains less than 100 observations")
     }
 
-    m_X <- as.matrix(df_tmp[, X_aug])
+    m_X <- as.matrix(df_tmp[, X_aug, drop = FALSE])
 
     tmp_fit <- stats::lm.fit(x = m_X, y = df_tmp[[y]])
 
-    list(coefficients = tmp_fit$coefficients,
-         r.squared    = R2FromLmFit(tmp_fit))
+    list(date         = tmp_date,
+         r.squared    = R2FromLmFit(tmp_fit),
+         coefficients = tmp_fit$coefficients)
   }
 
   l_cs_est <- lapply(v_dates, FUN = f_estimate)
 
-  # extract R^2
-  v_from_ll <- portfs::v_from_ll
-  
-  v_r_squared <- v_from_ll(l_cs_est, key = "r.squared")
+  stopifnot(length(l_cs_est) == n_dates)
 
-  # extract coefs
-  fGet <- function(l_elem){
-    l_elem[["coefficients"]]
+  # _______ data frame with cross-sectional estimates ______
+  # function to access elements of the list
+  f_get_elem <- function(l, elem){
+    l[[elem]]
   }
 
+  # start creating the data frame
+  df_cs_est <- data.frame(date      = vapply(l_cs_est,
+                                             FUN = f_get_elem,
+                                             FUN.VALUE = l_cs_est[[1]]$date,
+                                             elem = "date"),
+                          r.squared = vapply(l_cs_est,
+                                             FUN = f_get_elem,
+                                             FUN.VALUE = double(1),
+                                             elem = "r.squared"))
+
+  # rename the date column
+  names(df_cs_est)[1] <- date_var
+
+  # sanity check
+  stopifnot(nrow(df_cs_est) == n_dates)
+
+  # prepare coefficients for adding to the data frame
   m_coefs <- t(vapply(l_cs_est,
-                      FUN = fGet,
-                      FUN.VALUE = double(k))
+                      FUN = f_get_elem,
+                      FUN.VALUE = double(n_regressors),
+                      elem = "coefficients")
               )
 
   stopifnot(nrow(m_coefs) == n_dates)
-  stopifnot(all(X %in% colnames(m_coefs)))
+  stopifnot(ncol(m_coefs) == n_regressors)
+  stopifnot(all(X_aug %in% colnames(m_coefs)))
 
-  stopifnot(names(v_r_squared) == as.character(v_dates))
-  stopifnot(rownames(m_coefs)  == as.character(v_dates))
-
-  # make data frame with cross sectional estimates
-  df_cs_est <- data.frame(date = v_dates,
-                          r.squared = v_r_squared)
-
-  colnames(df_cs_est)[1] <- date_var
   df_cs_est <- cbind(df_cs_est, m_coefs)
 
-  # make Fama-MacBeth estimates
-  term_names <- colnames(m_coefs)
+  # ________________ Fama-MacBeth estimates ________________
+  for (j in seq_along(X_aug)){
+    tmp_name <- X_aug[j]
 
-  df_fmb_est <- list()
-
-  for (j in seq(1, k)){
-    tmp_y <- m_coefs[, j]
+    tmp_y <- df_cs_est[, tmp_name, drop = TRUE]
 
     tmp_fit <- stats::lm(tmp_y ~ 1)
 
     df_tmp <- broom::tidy(tmp_fit)
 
-    df_tmp$term <- term_names[j]
+    df_tmp$term <- tmp_name
 
-    df_fmb_est <- rbind(df_fmb_est, df_tmp)
+    if (j == 1){
+      df_fmb_est <- df_tmp
+    } else {
+      df_fmb_est <- rbind(df_fmb_est, df_tmp)
+    }
   }
 
   # return
