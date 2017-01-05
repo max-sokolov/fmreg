0bcdd45cfac2ff844d3c228dab7443ef53f53ca7
Maxim Sokolov <sokolov.maximus@gmail.com>
2017-01-05 12:41:56 -0500

    Rename qr_lm() to qr_lm_() to support the convention of naming of internal functions

--- R/qr_lm_.R
+++ R/qr_lm_.R
@@ -1,6 +1,6 @@
 # This is just a copy of the method qr.lm from stats package.
 
-qr_lm <- function(x, ...){
+qr_lm_ <- function(x, ...){
   if (is.null(r <- x$qr)){
     stop("lm object does not have a proper 'qr' component.\n Rank zero or should not have used lm(.., qr=FALSE).")
   } 
